2453283efca1bb0216c6fe6163f3f36475350f58
Bruno Guedes <brunoguedesit@gmail.com>
2020-12-09 07:23:40 -0300

    Simple query with query methods

--- src/main/java/com/brunoguedes/apiRestful/repositories/PostRepository.java
+++ src/main/java/com/brunoguedes/apiRestful/repositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.brunoguedes.apiRestful.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.brunoguedes.apiRestful.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
