c5e2ba8707d5f123686548e1382a689f1db508ca
Bruno Guedes <brunoguedesit@gmail.com>
2020-12-09 06:51:45 -0300

    Getting a post by id

---
+++ src/main/java/com/brunoguedes/apiRestful/services/PostService.java
@@ -0,0 +1,25 @@
+package com.brunoguedes.apiRestful.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunoguedes.apiRestful.domain.Post;
+import com.brunoguedes.apiRestful.repositories.PostRepository;
+import com.brunoguedes.apiRestful.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
+	
+	
